eb835c1e6b54b36e9cb6b59d1d51ae7f578492f9
djalal <djalal@users.noreply.github.com>
2021-05-26 16:14:15 +0200

    add specific scripts (#1)

---
+++ .artifakt/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo ">>>>>>>>>>>>>> START CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
+echo "------------------------------------------------------------"
+echo "The following build args are available:"
+env
+echo "------------------------------------------------------------"
+
+echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "

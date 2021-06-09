8d6e8ce0d7462be1bbfa68e4f48f5fb643957437
djalal <djalal@users.noreply.github.com>
2021-06-09 09:24:05 +0200

    migrate to cie github (#4)

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -10,12 +10,12 @@ echo "------------------------------------------------------------"
 
 RUNTIME_NAME="NodeJs"
 RUNTIME_LOGO="nodejs.svg"
-ARCHIVE_FILE=static-page-main
+ARCHIVE_FILE=main
 WORKDIR=/var/www/html/public
 
 env
 
-curl -sSLO https://gitlab.com/djalal/static-page/-/archive/main/${ARCHIVE_FILE}.tar.gz && \
+curl -sSLO https://github.com/artifakt-io/base-html/archive/refs/heads/${ARCHIVE_FILE}.tar.gz && \
   tar -xzf ${ARCHIVE_FILE}.tar.gz -C /tmp && \
   mv /tmp/${ARCHIVE_FILE}/* ${WORKDIR} && \
   chown -R www-data:www-data ${WORKDIR} && \
